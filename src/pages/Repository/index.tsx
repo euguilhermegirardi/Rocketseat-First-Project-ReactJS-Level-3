39a7f3f39f41bc6339eaf37568756dc89804dbde
Guilherme Girardi <girardi.gui@icloud.com>
2020-09-08 08:50:44 +0100

    Edit Reposiory

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
+import logoImg from "../../assets/logo.svg";
 
 interface RepositoryParams {
    repository: string;
@@ -8,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
    const { params } = useRouteMatch<RepositoryParams>();
 
-   return <h1>Repository: {params.repository}</h1>;
+   return (
+      <>
+         <Header>
+            <img src={logoImg} alt="logo.svg" />
+            <Link to="/">
+               <FiChevronLeft />
+               Go Back
+            </Link>
+         </Header>
+
+         <RepositoryInfo>
+            <header>
+               <img
+                  src="https://avatars3.githubusercontent.com/u/48716406?s=460&u=775b5cd15d0f20dc3dcc59b4a98b8d6f698d1085&v=4"
+                  alt="sdsd"
+               />
+               <div>
+                  <strong>Girardi</strong>
+                  <p>asdasd</p>
+               </div>
+            </header>
+            <ul>
+               <li>
+                  <strong>1890</strong>
+                  <span>Stars</span>
+               </li>
+               <li>
+                  <strong>34</strong>
+                  <span>Forks</span>
+               </li>
+               <li>
+                  <strong>43</strong>
+                  <span>Opened Issues</span>
+               </li>
+            </ul>
+         </RepositoryInfo>
+
+         <Issues>
+            <Link to="dasdas">
+               <div>
+                  <strong>asdasd</strong>
+                  <p>dasdasds</p>
+               </div>
+
+               <FiChevronRight size={20} />
+            </Link>
+         </Issues>
+      </>
+   );
 };
 
 export default Repository;
